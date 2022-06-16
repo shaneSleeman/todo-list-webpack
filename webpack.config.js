9ab5ff4b5fac8f39781f0089922da148981cc5ef
shaneSleeman <shanesleeman1@gmail.com>
2022-06-16 13:20:31 +1000

    Apply roboto font

--- webpack.config.js
+++ webpack.config.js
@@ -26,6 +26,10 @@ module.exports = {
             test: /\.css$/i,
             use: ['style-loader', 'css-loader'],
         },
+        {
+            test: /\.ttf$/i,
+            type: 'asset/resource',
+        }
     ],
   },
 };
