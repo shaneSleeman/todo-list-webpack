f159a1331db71ecc57a4d6ffda40dbb984c26c30
shaneSleeman <shanesleeman1@gmail.com>
2022-06-16 11:38:24 +1000

    Basic page layout

--- src/index.js
+++ src/index.js
@@ -1,8 +1,35 @@
 import './style.css';
-
+/*
 const body = document.querySelector("body");
 
 const title = document.createElement("h1");
 title.innerText = "Test";
 
 body.appendChild(title);
+*/
+
+const body = document.querySelector("body");
+
+const header = document.querySelector("div");
+header.classList.toggle("header");
+header.innerText = "Todo List!";
+
+const content = document.createElement("div");
+content.classList.toggle("content");
+
+const projects = document.createElement("div");
+projects.classList.toggle("projects");
+projects.innerText = ".";
+const cards = document.createElement("div");
+cards.classList.toggle("cards");
+cards.innerText = ".";
+content.appendChild(projects);
+content.appendChild(cards);
+
+const footer = document.createElement("div");
+footer.classList.toggle("footer");
+footer.innerText = "(c) Shane Sleeman 2022";
+
+body.appendChild(header);
+body.appendChild(content);
+body.appendChild(footer);
